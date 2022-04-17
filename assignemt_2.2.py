31417c04e18bfbbea4e9c0541ba11839213d1fe8
shantanubhardwaj1999 <91307782+shantanubhardwaj1999@users.noreply.github.com>
2022-04-17 17:53:38 +0530

    Add files via upload

---
+++ assignemt_2.2.py
@@ -0,0 +1,2 @@
+a=input("Enter the string you wanna reverse = ")
+print(a[::-1])
---
+++ assignment_2.1.py
@@ -0,0 +1,6 @@
+for i in range(0,1):
+    for n in range(0,6):
+        print("*"*n)
+for j in range(0,1):
+    for m in range(4,0,-1):
+        print("*"*m)
